f14b799ad4b896a718f20fed3216d6c137740dd3
DorinnB <dorinn@netcourrier.com>
2018-06-29 17:14:27 +0200

    schedule = NULL
    This reverts commit 22559c1ff18b69963eaf38f7f0e4359f09e010a2.

--- models/schedule-model.php
+++ models/schedule-model.php
@@ -25,7 +25,7 @@ class SCHEDULE
     $req='SELECT * from schedules
     LEFT JOIN techniciens ON techniciens.id_technicien=schedules.schedule_user
     WHERE id_info_job = (SELECT id_info_job FROM tbljobs WHERE id_tbljob = '.$id.')
-      AND schedule_actif = 1
+    AND schedule_actif = 1
     ORDER BY schedule_date DESC, id_schedule DESC
     ';
     //echo $req;
@@ -35,39 +35,40 @@ class SCHEDULE
 
   public function updateSchedule(){
     $reqUpdate='INSERT INTO schedules
-        (schedule_commentaire ,id_info_job, schedule_date, schedule_user)
-        VALUES
-        ( '.$this->schedule_commentaire.','.$this->id_info_job.','.$this->dateSchedule.', '.$this->db->quote($_COOKIE['id_user']).')
-        ';
-        //echo $reqUpdate;
-        $result = $this->db->query($reqUpdate);
-        //    $maReponse = array('result' => 'ok', 'req'=> $reqUpdate, 'info_jobs' => $this->getInfoJob()['id_info_job']);
-        //    			echo json_encode($maReponse);
+    (schedule_commentaire ,id_info_job, schedule_date, schedule_user)
+    VALUES
+    ( '.$this->schedule_commentaire.','.$this->id_info_job.','.$this->dateSchedule.', '.$this->db->quote($_COOKIE['id_user']).')
+    ';
+    //echo $reqUpdate;
+    $result = $this->db->query($reqUpdate);
+    //    $maReponse = array('result' => 'ok', 'req'=> $reqUpdate, 'info_jobs' => $this->getInfoJob()['id_info_job']);
+    //    			echo json_encode($maReponse);
 
-        $reqUpdate2='UPDATE info_jobs
-        SET schedule_recommendation = '.$this->schedule_recommendation.'
-        WHERE id_info_job = '.$this->id_info_job;
-      //echo $reqUpdate;
-            $result = $this->db->query($reqUpdate2);
-        //    $maReponse = array('result' => 'ok', 'req'=> $reqUpdate, 'info_jobs' => $this->getInfoJob()['id_info_job']);
-        //    			echo json_encode($maReponse);
+    $reqUpdate2='UPDATE info_jobs
+    SET schedule_recommendation = '.$this->schedule_recommendation.'
+    WHERE id_info_job = '.$this->id_info_job;
+    //echo $reqUpdate;
+    $result = $this->db->query($reqUpdate2);
+    //    $maReponse = array('result' => 'ok', 'req'=> $reqUpdate, 'info_jobs' => $this->getInfoJob()['id_info_job']);
+    //    			echo json_encode($maReponse);
   }
 
   public function updatescheduleInitial($date){
+    $date = ($date=="NULL")? "NULL" : $this->db->quote($date);
     $reqUpdate='UPDATE info_jobs
-    SET  	available_expected  = '.$this->db->quote($date).'
+    SET  	available_expected  = '.$date.'
     WHERE id_info_job = '.$this->id_info_job;
-//echo $reqUpdate;
-        $result = $this->db->execute($reqUpdate);
+    //echo $reqUpdate;
+    $result = $this->db->execute($reqUpdate);
   }
 
   public function updatescheduleSplit($type, $id, $date){
+    $date = ($date=="NULL")? "NULL" : $this->db->quote($date);
+    $reqUpdate='UPDATE tbljobs
+    SET '.$type.' = '.$date.'
+    WHERE id_tbljob = '.$this->db->quote($id);
 
-      $reqUpdate='UPDATE tbljobs
-      SET '.$type.' = '.$this->db->quote($date).'
-      WHERE id_tbljob = '.$this->db->quote($id);
-
-  //echo $reqUpdate;
+    //echo $reqUpdate;
     $result = $this->db->execute($reqUpdate);
   }
 
