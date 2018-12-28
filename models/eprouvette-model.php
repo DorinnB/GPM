c2fb0baef2c2f902418a16a7f1184f6026725ab5
DorinnB <dorinn@netcourrier.com>
2018-12-28 15:32:55 +0100

    add void dans report + suppression OLD

--- models/eprouvette-model.php
+++ models/eprouvette-model.php
@@ -512,7 +512,7 @@ class EprouvetteModel
       IF(Cycle_final is null,Cycle_final_temp, cycle_final) as Cycle_final, IF(Cycle_final is null,0, 1) as Cycle_final_valid,
       Rupture, Fracture, other_1, other_2, other_3, other_4, other_5,
       info_jobs.job, info_jobs.customer, split, test_type.id_test_type, test_type, test_type_abbr, eprouvettes.id_master_eprouvette, id_job,
-      signal_true, signal_tapered, young, flag_qualite, check_rupture, rawdatasent,
+      signal_true, signal_tapered, young, flag_qualite, check_rupture, rawdatasent, valid,
       d_commentaire, special_instruction, tbljob_instruction,
 
       IF(currentBlock is null,currentBlock_temp, currentBlock) as currentBlock,
