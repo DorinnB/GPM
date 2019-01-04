6f5df015b77df8b19e50d0c49fcb0240ad477745
DorinnB <dorinn@netcourrier.com>
2019-01-04 10:40:20 +0100

    Update phpmailer.php

--- test/phpmailer.php
+++ test/phpmailer.php
@@ -15,14 +15,14 @@ try {
     $mail->Host = 'smtp.office365.com';  // Specify main and backup SMTP servers
     $mail->SMTPAuth = true;
 
-    $mail->Username = 'pgonnet@metcut.com';                 // SMTP username
-    $mail->Password = '850pgo+15';                           // SMTP password
+    $mail->Username = '@metcut.com';                 // SMTP username
+    $mail->Password = '';                           // SMTP password
     $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
     $mail->Port = 587;                                    // TCP port to connect to
 
     //Recipients
-    $mail->setFrom('pgonnet@metcutfrance.com', 'GPM');
-    $mail->addAddress('pgonnet@metcutfrance.com');     // Add a recipient
+    $mail->setFrom('@metcutfrance.com', 'GPM');
+    $mail->addAddress('@metcutfrance.com');     // Add a recipient
 //    $mail->addAddress('ellen@example.com');               // Name is optional
     $mail->addReplyTo('do_not_reply@metcutfrance.com', 'GPM');
 //    $mail->addCC('cc@example.com');
