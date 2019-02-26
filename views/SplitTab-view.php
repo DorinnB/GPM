a56792cfef97898717e52d30584febd0685f672a
DorinnB <dorinn@netcourrier.com>
2019-02-26 16:31:27 +0100

    Update SplitTab-view.php

--- views/SplitTab-view.php
+++ views/SplitTab-view.php
@@ -1,9 +1,9 @@
 <link rel="stylesheet" href="css/splitTab.css">
 
-	<ul  class="nav nav-pills tab">
-		<?php foreach ($Tabs as $row): ?>
-			<li>
-				<a href="index.php?page=split&id_tbljob=<?= $row['id_tbljob'] ?>" style="background-color : <?= $row['statut_color'] ?>" class="<?=	$row['class']	?>"><?= $row['split'] ?>-<?= $row['test_type_abbr'] ?></a>
-			</li>
-		<?php endforeach ?>
-	</ul>
+<ul  class="nav nav-pills tab">
+	<?php foreach ($Tabs as $row): ?>
+		<li>
+			<a href="index.php?page=split&id_tbljob=<?= $row['id_tbljob'] ?>" style="background-color : <?= $row['statut_color'] ?>" class="<?=	$row['class']	?>"><acronym title="<?= $row['test_type'] ?>"><?= $row['split'] ?>-<?= $row['test_type_abbr'] ?></acronym></a>
+		</li>
+	<?php endforeach ?>
+</ul>
